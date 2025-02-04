de6b9093811d764a9e135335a21cf004048b3e58
Gustavo Esteves <gustavoesteves@directon.com.br>
2025-02-03 21:29:40 -0300

    feat: criação de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../pages/home/home.component';
+import { CreateTeacherComponent } from '../pages/create-teacher/create-teacher.component';
+import { CreateStudentComponent } from '../pages/create-student/create-student.component';
+import { LoginComponent } from '../pages/login/login.component';
+import { TeacherComponent } from '../pages/teacher/teacher.component';
+import { StudentComponent } from '../pages/student/student.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'teacher/:id',
+    component: TeacherComponent
+  },
+  {
+    path: 'create-teacher',
+    component: CreateTeacherComponent
+  },
+  {
+    path: 'student/:id',
+    component: StudentComponent
+  },
+  {
+    path: 'create-student',
+    component: CreateStudentComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/'
+  }
+];
