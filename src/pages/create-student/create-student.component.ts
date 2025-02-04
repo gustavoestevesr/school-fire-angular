b046014936c236f002bfbaea3e5bcac327a93112
Gustavo Esteves <gustavoesteves@directon.com.br>
2025-02-03 21:29:54 -0300

    feat: criação dos componentes de páginas

---
+++ src/pages/create-student/create-student.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-student',
+  imports: [],
+  templateUrl: './create-student.component.html',
+  styleUrl: './create-student.component.scss'
+})
+export class CreateStudentComponent {
+
+}
